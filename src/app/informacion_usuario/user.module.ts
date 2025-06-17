3ec23e959cdb46c3e8fff0dd93f0d63e0a0d5747
bahiron333 <bahiron39macro@gmail.com>
2025-06-17 17:23:34 -0500

    juego finalizado

---
+++ src/app/informacion_usuario/user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InformacionUsuarioComponent } from './informacion-usuario/informacion-usuario.component';
+
+
+
+@NgModule({
+  declarations: [
+    InformacionUsuarioComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
