83dc0815a3967c9d4486c3187ce577fe65ff0d4d
bahiron369 <duenas.bahiron@gmail.com>
2025-06-23 17:47:57 -0500

    configurar url servidor update 1

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    url:'https://preguntinaspnet.azurewebsites.net',
+    url:'https://preguntinaspnet20250623173137.azurewebsites.net',
     nombreApp:'PreguntinProd'
 }
